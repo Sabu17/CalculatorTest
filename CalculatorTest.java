2e9c1338ed77df828772d44647bb496dc7944f0a
Sabu17 <153213403+Sabu17@users.noreply.github.com>
2023-12-07 20:41:37 +0300

    Add files via upload

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,31 +1,45 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class CalculatorTest {
+  
+  @Test
+  public void addTest() {
+    assertEquals(5, CalculatorTest.add(2, 3), 0);
+  }
+ 
 
-  public static int multiply(int a, int b) {
-    return a * b;
+@Test
+  public void subtractTest() {
+    assertEquals(2, CalculatorTest.subtract(5, 3), 0);
   }
 
-  public static int divide(int a, int b) {
-    return a / b;
+  @Test
+  public void multiplyTest() {
+    assertEquals(12, CalculatorTest.multiply(3, 4), 0);
   }
-
+Asalblewi
+  @Test
+  public void divideTest() {
+    assertEquals(5, CalculatorTest.divide(10, 2), 0);
+  }
+  
   public static int subtract(int a, int b) {
     return a - b;
   }
+  
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+  
+  public static int divide(int a, int b) {
+    return a / b;
+  }
+}
+
 
   public static int add(int a, int b) {
     return a + b;
   }
 
-  public static void main(String[] args) {
-    System.out.println(CalculatorTest.subtract(5, 2));   // Output: 3
-    System.out.println(CalculatorTest.multiply(3, 4));   // Output: 12
-    System.out.println(CalculatorTest.divide(10, 2));    // Output: 5
-    System.out.println(CalculatorTest.add(7, 3));        // Output: 10
-  }
-}
-
-   
-
-    
-    
 
