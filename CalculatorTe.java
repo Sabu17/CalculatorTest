d87ea8a8f4af413464efbe3973405477153b6deb
Sabu17 <153213403+Sabu17@users.noreply.github.com>
2023-12-07 22:26:06 +0300

    Add files via upload

---
+++ CalculatorTe.java
@@ -0,0 +1,31 @@
+public class CalculatorTe {
+
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+
+  public static int divide(int a, int b) {
+    return a / b;
+  }
+
+  public static int subtract(int a, int b) {
+    return a - b;
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(CalculatorTe.subtract(5, 2));   // Output: 3
+    System.out.println(CalculatorTe.multiply(3, 4));   // Output: 12
+    System.out.println(CalculatorTe.divide(10, 2));    // Output: 5
+    System.out.println(CalculatorTe.add(7, 3));        // Output: 10
+  }
+}
+
+   
+
+    
+    
+
